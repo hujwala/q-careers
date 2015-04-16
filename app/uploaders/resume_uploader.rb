f270a22a0d2c36f347e0127604cc3803da04994f
Krishnaprasad Varma <kvarma@qwinixtech.com>
2015-04-16 16:57:12 +0530

    Carrier Wave Config for Production (Heroku)

--- app/uploaders/resume_uploader.rb
+++ app/uploaders/resume_uploader.rb
@@ -7,8 +7,7 @@ class ResumeUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage Rails.env.production? ? :fog : :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -39,7 +38,7 @@ class ResumeUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(pdf doc)
+    %w(pdf doc docx odt)
   end
 
   # Override the filename of the uploaded files:
