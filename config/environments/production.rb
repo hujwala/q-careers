6c3e92a9ea464080497b077bf3a0038ce021376c
anjankumarhn <anjangowdamandya@gmail.com>
2015-04-22 13:06:00 +0530

    For heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.asset_host = 'https://q-careers-dev.herokuapp.com'
   config.action_mailer.default_url_options = { :host => 'q-careers-dev.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
@@ -92,7 +92,7 @@ Rails.application.configure do
     :address => "smtp.sendgrid.net",
     :port => 587,
     :domain => "sendgrid.com",
-    :authentication => "plain",
+    :authentication => :login,
     :user_name =>"dputtannaiah",
     :password => "shuttle12"
   }
